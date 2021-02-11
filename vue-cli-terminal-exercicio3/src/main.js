973af442e896a9c99ebb8e2dc7d50fe17ae0b652
Everton Cabral <evertoncab@gmail.com>
2021-02-10 22:40:31 -0300

    criação dos componentes - etapa 1

--- vue-cli-terminal-exercicio3/src/main.js
+++ vue-cli-terminal-exercicio3/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+import './componentes';
+
+Vue.config.productionTip = false
+
+new Vue({
+    render: h => h(App),
+
+}).mount('#root')
